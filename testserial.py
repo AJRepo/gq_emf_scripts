a05a99f74179752b4569abffe36952382d619e63
AJRepo <AJRepo@users.noreply.github.com>
2023-05-18 16:59:01 -0500

    capture data to SPIR data boundary

--- testserial.py
+++ testserial.py
@@ -9,6 +9,13 @@ import binascii
 
 TEST_STRING = b'<GETVER>>'
 
+#data records come as a date row + 91 data rows 
+#91 rows at 14 bytes each + date row of 8 bytes
+#91*14 + 8 = 1274 + 8 = 502  (508 HEX)
+# Double = 2564 = 0A04 HEX
+# so we'll call <SPIR 00 00 00 0a 04>>
+# to get a two uninterrupted records
+
 
 port = serial.Serial('/dev/ttyUSB0', 115200 )
 
@@ -18,21 +25,22 @@ port.write( TEST_STRING )
 
 port.flush()
 
-response = port.read( 4098 )
+response = port.read( 2564 )
 
 print(response)
 
-TEST_STRING = bytearray(b'<SPIR\x00\x00\x00\x10\x00>>')
-TEST_STRING = b'<SPIR\x00\x00\x00\x10\x00>>'
+TEST_STRING = bytearray(b'<SPIR\x00\x00\x00\x10\x20>>')
+TEST_STRING = b'<SPIR\x00\x00\x00\x10\x20>>'
+TEST_STRING = b'<SPIR\x00\x00\x00\x0a\x04>>'
 
 port.write( TEST_STRING )
 
 port.flush()
 
-response = port.read( 4098 )
+response = port.read( 2564 )
 
-print(response)
-print("-----------------")
+#print(response)
+#print("-----------------")
 print(binascii.b2a_hex(response))
 
 port.close()
