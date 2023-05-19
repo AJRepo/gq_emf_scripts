9017d30cf3e149fd93989c647950474fedf85a8c
AJRepo <AJRepo@users.noreply.github.com>
2023-05-18 21:48:49 -0500

    documentation updates

--- README.md
+++ README.md
@@ -5,3 +5,49 @@ Debugging the Portable GQ EMF-390 tool led to the need to test
 via raw serial commands and to get back the raw data. 
 
 Simple scripts for testing that device
+
+
+TEST_STRING = b'<GETVER>>'
+
+Results in
+b'GQ-EMF390v2Re 3.72\r\n'
+
+---
+Data records come as 
+date row + N data rows (where N <= 180)
+date row + 180 data rows
+
+91 rows at 14 bytes each + date row of 8 bytes
+91 * 14 + 8 = 1274 + 8 = 502 DEC = 508 HEX
+ Two sets = 2564 DEC = 0A04 HEX
+ So call <SPIR 00 00 00 0a 04>>
+ to get a two uninterrupted records
+
+Dumping the first two timestamped data records
+TEST_STRING = b'<SPIR\x00\x00\x00\x0a\x04>>'
+
+55aa1705100e3737
+aa550013404f8042c43119485a02
+aa550013404f8042c43119485a02
+aa550013404f8042c43119485a02
+aa550013404f8042c43119485a02
+aa550013ce1f8142ab9c0b485a02
+aa5500148d567e42947efc475a08
+aa55001362cc8042947efc475a08
+aa55001365fd7e4280b721485a02
+aa55001321037e420e021e485a02
+... 82 more records ...
+55aa1705100e391a
+aa550013c1677b42f03f47485a08
+aa550013c1677b424fb745485a02
+aa550014eac07a42e60237485a02
+aa55001471b57c42545a4a485a08
+aa550014b5af7d429acb48485a08
+aa550014121a7a42652f41485a08
+aa550014990e7c4218194f485a08
+aa550014dd087d42545a4a485a08
+aa550013c1677b420e814d485a08
+... 170 more records ...
+55aa1705100f0019
+aa550013e3e47b42289a35485a02
+... etc ... 
--- testserial.py
+++ testserial.py
@@ -9,38 +9,43 @@ import binascii
 
 TEST_STRING = b'<GETVER>>'
 
-#data records come as a date row + 91 data rows 
-#91 rows at 14 bytes each + date row of 8 bytes
-#91*14 + 8 = 1274 + 8 = 502  (508 HEX)
-# Double = 2564 = 0A04 HEX
-# so we'll call <SPIR 00 00 00 0a 04>>
-# to get a two uninterrupted records
+#data records come as a date row + some number of data rows
+# until the first date record, then it is 180 data rows after
+#180 rows at 14 bytes each + date row of 8 bytes
+#180*14 + 8 = 2528 DEC = 9E0 HEX
+# call <SPIR 00 00 00 09 E0>>
+# to get at least one time record
+# max data per request = 65535 DEC = FFFF HEX (unless there's 
+# another undocumented change)
 
 
 port = serial.Serial('/dev/ttyUSB0', 115200 )
 
-port.timeout = 1.5
+port.timeout = 6
 
 port.write( TEST_STRING )
 
 port.flush()
 
-response = port.read( 2564 )
+response = port.read( 1024 )
 
 print(response)
 
 TEST_STRING = bytearray(b'<SPIR\x00\x00\x00\x10\x20>>')
-TEST_STRING = b'<SPIR\x00\x00\x00\x10\x20>>'
-TEST_STRING = b'<SPIR\x00\x00\x00\x0a\x04>>'
+TEST_STRING = b'<SPIR\x00\x00\x00\x09\xe0>>'
+TEST_STRING = b'<SPIR\x00\x00\x00\x12\x20>>'
+TEST_STRING = b'<SPIR\x00\x00\x00\xff\xff>>'
 
 port.write( TEST_STRING )
 
 port.flush()
 
-response = port.read( 2564 )
+response = port.read( 65535 )
 
+# Uncomment for raw data
 #print(response)
 #print("-----------------")
+# print as ascii-expanded hex
 print(binascii.b2a_hex(response))
 
 port.close()
